0d9ba8303012f1e089e4db4a9b9a56a531539591
rvon4 <rvon4@student.monash.edu>
2016-10-05 17:43:28 +1100

    Update Property.php

--- Property.php
+++ Property.php
@@ -17,7 +17,6 @@
 				<th>Bathrooms</th>
 				<th>Description</th>
 				<th>Type</th>
-				<th>View</th>
 				<th>Edit</th>
 				<th>Delete</th>
 			</tr>
@@ -60,7 +59,6 @@
 					<td><?php echo $propRow["PROP_DESCRIPTION"]; ?></td>
 					<td><?php echo $propType; ?></td>
 					
-					<td><a href = "PropModify.php?propid=<?php echo $propRow["PROP_ID"]?>&Action=View">View</a></td>
 					<td><a href = "PropModify.php?propid=<?php echo $propRow["PROP_ID"]?>&Action=Update">Edit</a></td>
 					<td><a href = "PropModify.php?propid=<?php echo $propRow["PROP_ID"]?>&Action=Delete">Delete </a></td>
 				</tr>
